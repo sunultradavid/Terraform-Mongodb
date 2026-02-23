d80cc54b59f6c1846d3608e9ec23848d2b261932
David Sunday Famuyiwa <david@scripictural.com>
2026-02-23 12:05:42 +0000

    Change default AWS region to eu-west-1

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "project_name" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-west-1"
 }
 
 # Networking variables
