9222220bb502c2e130c2e02f8b8b6437d59c5664
David Sunday Famuyiwa <david@scripictural.com>
2026-02-23 11:17:16 +0000

    Change S3 backend region from eu-west-1 to us-east-1

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket"
     key            = "fullstack-app/terraform.tfstate"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
